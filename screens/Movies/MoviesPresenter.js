823c9c6828ee7cf5c0792c24df9853d00ad80735
okdohyuk <okdohyuk2@gmail.com>
2020-04-16 15:37:38 +0900

    #5.0 Search

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import Swiper from 'react-native-web-swiper';
-import { ScrollView, Dimensions } from 'react-native';
+import { Dimensions } from 'react-native';
 import Slide from '../../components/Movies/Slide';
-import Title from '../../components/Title';
 import Vertical from '../../components/Vertical';
 import Horizontal from '../../components/Horizontal';
 import ScrollContainer from '../../components/ScrollContainer';
