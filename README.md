2982d6abe778d154e159d9c4986efd6bb9f9f998
okdohyuk <okdohyuk2@gmail.com>
2020-04-18 17:55:05 +0900

    #7.2 Finishing the Discovery

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Movies app
+# MovieSearch
 
 iOS / Android Movie Discovery App built with React Native.
--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -26,33 +26,59 @@ const styles = {
 
 export default ({ results }) => {
   const [topIndex, setTopIndex] = useState(0);
+  const nextCard = () => setTopIndex((currentValue) => currentValue + 1);
   const position = new Animated.ValueXY();
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (evt, { dx, dy }) => {
       position.setValue({ x: dx, y: dy });
     },
-    onPanResponderRelease: () => {
-      Animated.spring(position, {
-        toValue: {
-          x: 0,
-          y: 0,
-        },
-      }).start();
+    onPanResponderRelease: (evt, { dx, dy }) => {
+      if (dx >= 250) {
+        Animated.spring(position, {
+          toValue: {
+            x: WIDTH + 100,
+            y: dy,
+          },
+        }).start(nextCard);
+      } else if (dx <= -250) {
+        Animated.spring(position, {
+          toValue: {
+            x: -WIDTH - 100,
+            y: dy,
+          },
+        }).start(nextCard);
+      } else {
+        Animated.spring(position, {
+          toValue: {
+            x: 0,
+            y: 0,
+          },
+        }).start();
+      }
     },
   });
   const roationValues = position.x.interpolate({
-    inputRange: [-100, 0, 100],
-    outputRange: ['-5deg', '0deg', '5deg'],
+    inputRange: [-200, 0, 255],
+    outputRange: ['-8deg', '0deg', '8deg'],
     extrapolate: 'clamp',
   });
-  setInterval(() => {
-    console.log(roationValues);
-  }, 500);
+  const secondCardOpacity = position.x.interpolate({
+    inputRange: [-255, 0, 255],
+    outputRange: [1, 0.2, 1],
+    extrapolate: 'clamp',
+  });
+  const secondCardScale = position.x.interpolate({
+    inputRange: [-255, 0, 255],
+    outputRange: [1, 0.8, 1],
+    extrapolate: 'clamp',
+  });
   return (
     <Container>
       {results.map((results, index) => {
-        if (index === topIndex) {
+        if (index < topIndex) {
+          return null;
+        } else if (index === topIndex) {
           return (
             <Animated.View
               style={{
@@ -69,19 +95,36 @@ export default ({ results }) => {
               <Poster source={{ uri: apiImage(results.poster_path) }} />
             </Animated.View>
           );
+        } else if (index === topIndex + 1) {
+          return (
+            <Animated.View
+              style={{
+                ...styles,
+                zIndex: -index,
+                opacity: secondCardOpacity,
+                transform: [{ scale: secondCardScale }],
+              }}
+              key={results.id}
+              {...panResponder.panHandlers}
+            >
+              <Poster source={{ uri: apiImage(results.poster_path) }} />
+            </Animated.View>
+          );
+        } else {
+          return (
+            <Animated.View
+              style={{
+                ...styles,
+                zIndex: -index,
+                opacity: 0,
+              }}
+              key={results.id}
+              {...panResponder.panHandlers}
+            >
+              <Poster source={{ uri: apiImage(results.poster_path) }} />
+            </Animated.View>
+          );
         }
-        return (
-          <Animated.View
-            style={{
-              ...styles,
-              zIndex: -index,
-            }}
-            key={results.id}
-            {...panResponder.panHandlers}
-          >
-            <Poster source={{ uri: apiImage(results.poster_path) }} />
-          </Animated.View>
-        );
       })}
     </Container>
   );
