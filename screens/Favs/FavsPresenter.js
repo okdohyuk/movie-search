23e4a34f173858c44b4bdfc07802eaebe43fc098
okdohyuk <okdohyuk2@gmail.com>
2020-04-18 15:39:59 +0900

    #7.1 Roation the Cards

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PanResponder, Dimensions, Animated } from 'react-native';
 import styled from 'styled-components/native';
 import { apiImage } from '../../api';
@@ -25,27 +25,64 @@ const styles = {
 };
 
 export default ({ results }) => {
+  const [topIndex, setTopIndex] = useState(0);
   const position = new Animated.ValueXY();
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (evt, { dx, dy }) => {
       position.setValue({ x: dx, y: dy });
     },
+    onPanResponderRelease: () => {
+      Animated.spring(position, {
+        toValue: {
+          x: 0,
+          y: 0,
+        },
+      }).start();
+    },
   });
+  const roationValues = position.x.interpolate({
+    inputRange: [-100, 0, 100],
+    outputRange: ['-5deg', '0deg', '5deg'],
+    extrapolate: 'clamp',
+  });
+  setInterval(() => {
+    console.log(roationValues);
+  }, 500);
   return (
     <Container>
-      {results.reverse().map((results) => (
-        <Animated.View
-          style={{
-            ...styles,
-            transform: [...position.getTranslateTransform()],
-          }}
-          key={results.id}
-          {...panResponder.panHandlers}
-        >
-          <Poster source={{ uri: apiImage(results.poster_path) }} />
-        </Animated.View>
-      ))}
+      {results.map((results, index) => {
+        if (index === topIndex) {
+          return (
+            <Animated.View
+              style={{
+                ...styles,
+                zIndex: 1,
+                transform: [
+                  { rotate: roationValues },
+                  ...position.getTranslateTransform(),
+                ],
+              }}
+              key={results.id}
+              {...panResponder.panHandlers}
+            >
+              <Poster source={{ uri: apiImage(results.poster_path) }} />
+            </Animated.View>
+          );
+        }
+        return (
+          <Animated.View
+            style={{
+              ...styles,
+              zIndex: -index,
+            }}
+            key={results.id}
+            {...panResponder.panHandlers}
+          >
+            <Poster source={{ uri: apiImage(results.poster_path) }} />
+          </Animated.View>
+        );
+      })}
     </Container>
   );
 };
